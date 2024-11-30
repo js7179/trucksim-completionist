e4a0bbe2f30b2686acf226c093752d6981193e2e
js7179 <44449056+js7179@users.noreply.github.com>
2024-11-30 17:35:05 -0500

    handle expired jwts gracefully

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { createSecretKey } from 'crypto';
 import { Request, Response, NextFunction } from 'express';
 import { jwtVerify } from 'jose';
+import { JOSEError, JWTExpired } from 'jose/errors';
 
 const jwtSecret = createSecretKey(process.env.JWT_SECRET, 'utf-8');
 
@@ -19,7 +20,18 @@ export default async function AuthenticationHeader(req: Request, res: Response, 
 
     const jwt = split[1];
 
-    const { payload } = await jwtVerify(jwt, jwtSecret);
-    res.locals.uuid = payload.sub;
+    try {
+        const { payload } = await jwtVerify(jwt, jwtSecret);
+        res.locals.uuid = payload.sub;
+    } catch (err) {
+        if(err instanceof JOSEError) {
+            const joseErr = err as JOSEError;
+            if(joseErr instanceof JWTExpired) {
+                return res.status(401).send('JWT in Authorization header has expired');
+            }
+        } else {
+            return res.status(500).send('Unexpected failure verifying JWT');
+        }
+    }
     next();
 }

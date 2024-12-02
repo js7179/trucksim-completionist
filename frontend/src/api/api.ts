42b190e9c0a8ec679619e7ce97701fae18f209cd
js7179 <44449056+js7179@users.noreply.github.com>
2024-12-01 20:40:50 -0500

    using axios instance interceptor to attach authorization header

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,22 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+import { auth } from '@/supabase';
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+async function authorizationHeader(config: InternalAxiosRequestConfig) {
+    const sessionData = (await auth.getSession()).data.session;
+    if(sessionData === null) {
+        return config;
+    }
+    config.headers.Authorization = `Bearer ${sessionData.access_token}`;
+    return config;
+}
+
+api.interceptors.request.use(authorizationHeader);
+
+export async function testUser(): Promise<string> {
+    const res = await api.get('/testauth');
+    return res.data.uuid as string;
+}
--- frontend/src/components/auth/APIAuthTest.tsx
+++ frontend/src/components/auth/APIAuthTest.tsx
@@ -1,22 +1,13 @@
-import { useAuth } from "@/hooks/Auth";
-import axios from 'axios';
 import { useState } from "react";
+import { testUser } from "@/api/api";
 
 export default function APIAuthTest() {
-    const { session } = useAuth();
     const [body, setBody] = useState<string>('');
     
     const onClick = async () => {
-        const url = new URL('/testauth', import.meta.env.VITE_API_URL).href;
-        
-        const authHeader = session ? `Bearer ${session.access_token}` : 'Bearer Bearer Bearer';
         try {
-            const res = await axios.get(url, {
-                headers: {
-                    Authorization: authHeader
-                }
-            });
-            setBody(`Your UUID is ${res.data.uuid}!`);
+            const res = await testUser();
+            setBody(`Your UUID is ${res}!`);
         } catch (err) {
             const error = err as Error;
             setBody(error.message);

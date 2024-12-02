c7b9c3e4bf98b45eb2c90bf607d4b140adeeab5f
js7179 <44449056+js7179@users.noreply.github.com>
2024-12-01 20:16:30 -0500

    more jwt verification error and middleware rename

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from 'cors';
-import AuthenticationHeader from "./middleware/auth";
+import AuthorizationHeaderMiddleware from "./middleware/auth";
 
 const app = express();
 app.use(cors());
+app.disable('x-powered-by');
+app.set('etag', false);
 
 const port = process.env.PORT || 3500;
 
@@ -13,7 +15,7 @@ app.get("/ping", (req, res) => {
     });
 });
 
-app.get("/testauth", AuthenticationHeader, (req, res) => {
+app.get("/testauth", AuthorizationHeaderMiddleware, (req, res) => {
     res.status(200).send({ uuid: res.locals.uuid });
 });
 
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,11 +1,11 @@
 import { createSecretKey } from 'crypto';
 import { Request, Response, NextFunction } from 'express';
 import { jwtVerify } from 'jose';
-import { JOSEError, JWTExpired } from 'jose/errors';
+import { JOSEError, JWSInvalid, JWSSignatureVerificationFailed, JWTClaimValidationFailed, JWTExpired } from 'jose/errors';
 
 const jwtSecret = createSecretKey(process.env.JWT_SECRET, 'utf-8');
 
-export default async function AuthenticationHeader(req: Request, res: Response, next: NextFunction) {
+export default async function AuthorizationHeaderMiddleware(req: Request, res: Response, next: NextFunction) {
     const authorizationHeader = req.headers.authorization;
     if(!authorizationHeader) {
         console.log('Request made with no Authorization header');
@@ -21,13 +21,22 @@ export default async function AuthenticationHeader(req: Request, res: Response, 
     const jwt = split[1];
 
     try {
-        const { payload } = await jwtVerify(jwt, jwtSecret);
+        const { payload } = await jwtVerify(jwt, jwtSecret, {
+            issuer: process.env.JWT_ISS,
+            audience: 'authenticated'
+        });
         res.locals.uuid = payload.sub;
     } catch (err) {
         if(err instanceof JOSEError) {
             const joseErr = err as JOSEError;
             if(joseErr instanceof JWTExpired) {
                 return res.status(401).send('JWT in Authorization header has expired');
+            } else if (joseErr instanceof JWSInvalid || joseErr instanceof JWSSignatureVerificationFailed) {
+                return res.status(400).send('Invalid JWT in Authorization header');
+            } else if(joseErr instanceof JWTClaimValidationFailed) {
+                return res.status(401).send('JWT issuer or audience does not match what was expected');
+            } else {
+                return res.status(500).send('Unknown error verifying JWT');
             }
         } else {
             return res.status(500).send('Unexpected failure verifying JWT');

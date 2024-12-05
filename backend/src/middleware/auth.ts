9fefd4e1eceab382f530d937d86ec626c2c15eb9
js7179 <44449056+js7179@users.noreply.github.com>
2024-12-05 06:21:10 -0500

    on failure, send back as json per Google JSON Style Guide instead of html string

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -8,14 +8,12 @@ const jwtSecret = createSecretKey(process.env.JWT_SECRET, 'utf-8');
 export default async function AuthorizationHeaderMiddleware(req: Request, res: Response, next: NextFunction) {
     const authorizationHeader = req.headers.authorization;
     if(!authorizationHeader) {
-        console.log('Request made with no Authorization header');
-        return res.status(401).send('No authorization header');
+        return res.status(401).json({error: { message: 'No authorization header'}});
     }
 
     const split = authorizationHeader.split(' ');
     if(split.length !== 2) {
-        console.log('Request made with malformed Authorization header');
-        return res.status(400).send("Expected Authorization header to be of form 'Bearer [jwt]'");
+        return res.status(400).json({ error: { message: "Expected Authorization header to be of form 'Bearer [jwt]" } });
     }
 
     const jwt = split[1];
@@ -30,16 +28,16 @@ export default async function AuthorizationHeaderMiddleware(req: Request, res: R
         if(err instanceof JOSEError) {
             const joseErr = err as JOSEError;
             if(joseErr instanceof JWTExpired) {
-                return res.status(401).send('JWT in Authorization header has expired');
+                return res.status(401).json({ error: { message: 'JWT in Authorization header has expired' } });
             } else if (joseErr instanceof JWSInvalid || joseErr instanceof JWSSignatureVerificationFailed) {
-                return res.status(400).send('Invalid JWT in Authorization header');
+                return res.status(400).json({ error: { message: 'Invalid JWT in Authorization header' } });
             } else if(joseErr instanceof JWTClaimValidationFailed) {
-                return res.status(401).send('JWT issuer or audience does not match what was expected');
+                return res.status(401).json({ error: { message: 'JWT issuer or audience does not match what was expected' } });
             } else {
-                return res.status(500).send('Unknown error verifying JWT');
+                return res.status(500).json({ error: { message: 'Unknown error verifying JWT' } });
             }
         } else {
-            return res.status(500).send('Unexpected failure verifying JWT');
+            return res.status(500).json({ error: { message: 'Unexpected failure verifying JWT' } });
         }
     }
     next();

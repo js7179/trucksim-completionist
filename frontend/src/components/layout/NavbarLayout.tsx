926ba633314ca296feb1ab0e095255bc96cf9c17
js7179 <44449056+js7179@users.noreply.github.com>
2025-06-02 19:13:20 -0400

    add location as key to suspense to force loading spinner to appear on page change

--- frontend/src/components/layout/NavbarLayout.tsx
+++ frontend/src/components/layout/NavbarLayout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "./Navbar";
 import { Suspense } from "react";
 import LoadingSpinner from "../util/LoadingSpinner";
 
 export default function NavbarLayout() {
+    const location = useLocation();
+
     return(
         <>
             <Navbar />
-            <Suspense fallback={<LoadingSpinner />}>
+            <Suspense key={location.key} fallback={<LoadingSpinner />}>
                 <Outlet />
             </Suspense>
         </>
